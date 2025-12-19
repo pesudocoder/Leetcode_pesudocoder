eb486420f48da98a01c2e9d982da42f214d4baa5
pesudocoder <154416308+pesudocoder@users.noreply.github.com>
2025-12-19 15:25:55 +0530

    Create 31-NextPermutation

---
+++ 31-NextPermutation/31-NextPermutation.cpp
@@ -0,0 +1,8 @@
+// Last updated: 12/19/2025, 3:25:55 PM
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(),nums.end());
+        
+    }
+};
