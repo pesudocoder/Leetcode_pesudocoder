fd1574d0e67fb021b8b37f88e0a41153daf57a39
pesudocoder <154416308+pesudocoder@users.noreply.github.com>
2026-01-20 22:47:13 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.cpp
@@ -0,0 +1,26 @@
+// Last updated: 1/20/2026, 10:47:13 PM
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('||s[i]=='['||s[i]=='{'){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()) return false;
+                char ch=st.top();
+                st.pop();
+                if((s[i]==')'&& ch=='(')||(s[i]==']'&& ch=='[')||(s[i]=='}'&& ch=='{')){
+                    continue;
+                }
+                else return false;
+            }
+
+            
+            
+        }
+        return st.empty();
+        
+    }
+};
