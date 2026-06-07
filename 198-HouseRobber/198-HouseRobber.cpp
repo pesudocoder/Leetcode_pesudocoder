5a41967ef4fb5e57841e181ec8d335262eaf377c
pesudocoder <154416308+pesudocoder@users.noreply.github.com>
2026-06-07 13:17:31 +0530

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.cpp
@@ -0,0 +1,18 @@
+// Last updated: 6/7/2026, 1:17:31 PM
+class Solution {
+public:
+    int maxsum(int ind , vector<int> & nums,vector<int>& dp){
+        if(ind==0) return nums[ind];
+        if(ind<0) return 0;
+        if(dp[ind]!=-1) return dp[ind];
+        int pick = nums[ind] + maxsum(ind-2,nums,dp);
+        int notpick=0+ maxsum(ind-1,nums,dp);
+        return dp[ind]= max(pick,notpick);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n,-1);
+        return maxsum(n-1,nums,dp);
+        
+    }
+};
