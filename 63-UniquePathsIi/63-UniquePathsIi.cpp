f26a45bf6fb6a89ceae2ceb154b6e7129dce2379
pesudocoder <154416308+pesudocoder@users.noreply.github.com>
2026-06-08 14:43:55 +0530

    Create 63-UniquePathsIi

---
+++ 63-UniquePathsIi/63-UniquePathsIi.cpp
@@ -0,0 +1,22 @@
+// Last updated: 6/8/2026, 2:43:53 PM
+class Solution {
+public:
+    int f(int i , int j , vector<vector<int>>& o , vector<vector<int>>& dp){
+        if(i<0 || j<0) return 0;
+        if( o[i][j]==1) return 0;
+        if(i==0 && j==0) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up = f(i-1,j,o,dp);
+        int left = f(i,j-1,o,dp);
+        return dp[i][j] = up+left;
+    }
+
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        if(obstacleGrid[0][0]==1 || obstacleGrid[m-1][n-1]==1) return 0;
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return f(m-1,n-1,obstacleGrid,dp);
+        
+    }
+};
