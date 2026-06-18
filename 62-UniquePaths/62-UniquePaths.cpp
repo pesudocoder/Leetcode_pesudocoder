003a7aeedf5ad94c3a2259a727d7c6a95425934d
pesudocoder <154416308+pesudocoder@users.noreply.github.com>
2026-06-18 12:44:41 +0530

    Create 62-UniquePaths

---
+++ 62-UniquePaths/62-UniquePaths.cpp
@@ -0,0 +1,27 @@
+// Last updated: 6/18/2026, 12:44:40 PM
+class Solution {
+public:
+    // int f(int i , int j , vector<vector<int>> & dp ){
+    //     if(i==0 && j==0) return 1;
+    //     if(i<0 || j <0) return 0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     int left = f(i,j-1,dp);
+    //     int right = f(i-1,j,dp);
+    //     return dp[i][j]= left +right;
+    // }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp (m,vector<int>(n,-1));
+        dp[0][0] = 1;
+        for(int i = 0 ; i < m ; i ++){
+            for(int j = 0 ; j < n ; j++){
+                if(i==0 && j==0) continue;
+                int up = 0 ,left =0;
+                if(i>0) up = dp[i-1][j];
+                if(j>0) left = dp[i][j-1];
+                dp[i][j]=up+left;
+            }
+        }
+        return dp[m-1][n-1];
+        
+    }
+};
